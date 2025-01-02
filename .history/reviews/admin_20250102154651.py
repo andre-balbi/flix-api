ecf94e09be289c4a38d0b5672ace50014a43c484
drebalbi <abalbiaguiar@gmail.com>
2025-01-02 15:59:12 -0300

    remove unused historical files for actors, genres, movies, and reviews

--- .history/reviews/admin_20250102154651.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from reviews.models import Review
-
-
-class ReviewAdmin(Review):
-    list_display = ("id", "movie", "stars")
-
-
-# Register your models here.
--- .history/reviews/models_20250102152724.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Review(models.Model):
-    movie = models.ForeignKey(
-        "movies.Movie", on_delete=models.PROTECT, related_name="reviews"
-    )
-    stars = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
-    comment = models.TextField(max_length=300, null=True, blank=True)
