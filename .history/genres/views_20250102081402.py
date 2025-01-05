a44657d2620bfc489925f1c29bc33c6ba8ec79c9
drebalbi <abalbiaguiar@gmail.com>
2025-01-05 14:47:08 -0300

    remove historical files for actors, genres, movies, and reviews

--- .history/genres/views_20250102081402.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-from genres.models import Genre
-
-
-def genre_view(request):
-    genres = Genre.objects.all()
-    return JsonResponse({genres})
--- .history/movies/serializers_20250103110017.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from movies.models import Movie
-
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = "__all__"
